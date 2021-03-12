f348bec9c35255096c7b06a23d89887da416eb3f
chengwf <danielchengwf@outlook.com>
2021-03-12 15:24:59 +0800

    纯数字感觉不太好，还是int加个L转long类型稳妥点

--- utils/src/main/java/com/chengwf/utils/ext/IntExt.kt
+++ utils/src/main/java/com/chengwf/utils/ext/IntExt.kt
@@ -6,23 +6,3 @@ import java.util.concurrent.TimeUnit
 
 
 fun Int.toPix() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), Resources.getSystem().displayMetrics)
-
-/**
- * 将秒钟单位long值转成毫秒
- */
-fun Int.second() = TimeUnit.SECONDS.toMillis(this.toLong())
-
-/**
- * 将分钟单位long值转成毫秒
- */
-fun Int.minute() = TimeUnit.MINUTES.toMillis(this.toLong())
-
-/**
- * 将小时单位long值转成毫秒
- */
-fun Int.hour() = TimeUnit.HOURS.toMillis(this.toLong())
-
-/**
- * 将天单位long值转成毫秒
- */
-fun Int.day() = TimeUnit.DAYS.toMillis(this.toLong())
