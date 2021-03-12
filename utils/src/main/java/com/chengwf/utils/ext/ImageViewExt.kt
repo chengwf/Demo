fd2d628f4c52591cec89bfd557fdc1b211879266
chengwf <danielchengwf@outlook.com>
2021-03-12 15:26:27 +0800

    去掉option，暂时还不知道两个的区别，需要用的时候自行甄别吧

--- utils/src/main/java/com/chengwf/utils/ext/ImageViewExt.kt
+++ utils/src/main/java/com/chengwf/utils/ext/ImageViewExt.kt
@@ -3,28 +3,27 @@ package com.chengwf.utils.ext
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 
 /**
  * 多了个error
  */
 fun ImageView.load(url: String?) =
         Glide.with(this).load(url)
-                .apply(RequestOptions().centerCrop())
+                .centerCrop()
                 .into(this)
 
 fun ImageView.load(@DrawableRes resId: Int) =
         Glide.with(this).load(resId)
-                .apply(RequestOptions().centerCrop())
+                .centerCrop()
                 .into(this)
 
 
 fun ImageView.loadCircle(url: String?) =
         Glide.with(this).load(url)
-                .apply(RequestOptions.circleCropTransform())
+                .circleCrop()
                 .into(this)
 
 fun ImageView.loadCircle(@DrawableRes resId: Int) =
         Glide.with(this).load(resId)
-                .apply(RequestOptions.circleCropTransform())
+                .circleCrop()
                 .into(this)
