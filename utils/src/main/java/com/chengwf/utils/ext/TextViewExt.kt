9d948390926c9bff6b571b9aa95d7b31012d5a39
chengwf <danielchengwf@outlook.com>
2021-03-12 15:27:38 +0800

    代码设置TextView的drawable（drawableTop，drawableBottom。。。）

--- utils/src/main/java/com/chengwf/utils/ext/TextViewExt.kt
+++ utils/src/main/java/com/chengwf/utils/ext/TextViewExt.kt
@@ -1,15 +1,20 @@
 package com.chengwf.utils.ext
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.widget.TextView
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
+import androidx.annotation.Size
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.chengwf.utils.R
+import kotlin.math.min
 
 fun TextView.string() = this.text.toString()
 fun TextView.int() = this.string().run {
@@ -26,8 +31,12 @@ fun TextView.float() = this.string().toFloat()
 /**
  * 设置颜色直接使用colors.xml中定义的颜色即可
  */
-fun TextView.setColor(resId: Int) {
-    this.setTextColor(resources.getColor(resId))
+fun TextView.setColor(@ColorRes resId: Int) {
+    this.setTextColor(ContextCompat.getColor(context, resId))
+}
+
+fun TextView.setColor(@Size(min = 4) colorStr: String) {
+    this.setTextColor(Color.parseColor(colorStr))
 }
 
 fun TextView.drawableTop(@DrawableRes resId: Int) {
@@ -56,6 +65,23 @@ fun TextView.drawableEnd(@DrawableRes resId: Int) {
 
 fun TextView.drawableEnd(url: String) {
 
+    urlToBitmap(context, url) { bitmap ->
+        val drawable = BitmapDrawable(resources, bitmap).apply {
+            setBounds(0, 0, minimumWidth, minimumHeight)
+        }
+        this@drawableEnd.setCompoundDrawables(null, null, drawable, null)
+    }
+}
+
+fun TextView.drawableBottom(@DrawableRes resId: Int) {
+
+    ContextCompat.getDrawable(context, resId)?.let {
+        it.setBounds(0, 0, it.minimumWidth, it.minimumHeight)
+        this.setCompoundDrawables(null, null, null, it)
+    }
+}
+
+fun urlToBitmap(context: Context, url: String, func: (Bitmap) -> Unit) {
     Glide.with(context)
         .asBitmap()
         .circleCrop()
@@ -67,19 +93,7 @@ fun TextView.drawableEnd(url: String) {
             }
 
             override fun onResourceReady(bitmap: Bitmap, transition: Transition<in Bitmap>?) {
-
-                val drawable = BitmapDrawable(resources, bitmap).apply {
-                    setBounds(0, 0, minimumWidth, minimumHeight)
-                }
-                this@drawableEnd.setCompoundDrawables(null, null, drawable, null)
+                func(bitmap)
             }
         })
-}
-
-fun TextView.drawableBottom(@DrawableRes resId: Int) {
-
-    ContextCompat.getDrawable(context, resId)?.let {
-        it.setBounds(0, 0, it.minimumWidth, it.minimumHeight)
-        this.setCompoundDrawables(null, null, null, it)
-    }
 }
